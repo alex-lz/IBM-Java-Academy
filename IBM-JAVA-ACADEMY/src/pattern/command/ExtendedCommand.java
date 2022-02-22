374df73b831e40ebd506e111e35fe9a1fe090263
alex-lz <alexalm2495@gmail.com>
2022-02-22 10:26:22 -0700

    Create class extendedcommand

---
+++ IBM-JAVA-ACADEMY/src/pattern/command/ExtendedCommand.java
@@ -0,0 +1,21 @@
+package pattern.command;
+
+import java.time.Duration;
+
+public class ExtendedCommand extends Command {
+
+	public ExtendedCommand(Duration duration) {
+		super(duration);
+	}
+	
+	@Override
+	public void run() {
+		System.out.println("Hello from extended command!");
+	}
+	
+	public final static void main(String[] args) {
+		Command command = new ExtendedCommand(Duration.parse("pt1s"));
+		command.run();
+	}
+
+}
